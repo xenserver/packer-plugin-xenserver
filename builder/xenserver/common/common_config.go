makunterry/packer-builder-xenserver

CP-18743: Make packer-build-xenserver support VIF configuration
Signed-off-by: kunm <kun.ma@citrix.com>

builder/xenserver/common/common_config.go
@@ -18,11 +18,11 @@ type CommonConfig struct {
 	Password string `mapstructure:"remote_password"`
 	HostIp   string `mapstructure:"remote_host"`
 
-	VMName        string   `mapstructure:"vm_name"`
-	VMDescription string   `mapstructure:"vm_description"`
-	SrName        string   `mapstructure:"sr_name"`
-	FloppyFiles   []string `mapstructure:"floppy_files"`
-	NetworkName   string   `mapstructure:"network_name"`
+	VMName        string            `mapstructure:"vm_name"`
+	VMDescription string            `mapstructure:"vm_description"`
+	SrName        string            `mapstructure:"sr_name"`
+	FloppyFiles   []string          `mapstructure:"floppy_files"`
+	NetworkName   map[string]string `mapstructure:"network_name"`
 
 	HostPortMin uint `mapstructure:"host_port_min"`
 	HostPortMax uint `mapstructure:"host_port_max"`

builder/xenserver/iso/step_create_instance.go
@@ -109,7 +109,7 @@ func (self *stepCreateInstance) Run(state multistep.StateBag) multistep.StepActi
 
 	var network *xsclient.Network
 
-	if config.NetworkName == "" {
+	if len(config.NetworkName) == 0 {
 		// No network has be specified. Use the management interface
 		network = new(xsclient.Network)
 		network.Ref = ""
@@ -141,35 +141,39 @@ func (self *stepCreateInstance) Run(state multistep.StateBag) multistep.StepActi
 			return multistep.ActionHalt
 		}
 
-	} else {
-		// Look up the network by it's name label
-
-		networks, err := client.GetNetworkByNameLabel(config.NetworkName)
+		_, err = instance.ConnectNetwork(network, "0")
 
 		if err != nil {
-			ui.Error(fmt.Sprintf("Error occured getting Network by name-label: %s", err.Error()))
-			return multistep.ActionHalt
+			ui.Say(err.Error())
 		}
 
-		switch {
-		case len(networks) == 0:
-			ui.Error(fmt.Sprintf("Couldn't find a network with the specified name-label '%s'. Aborting.", config.NetworkName))
-			return multistep.ActionHalt
-		case len(networks) > 1:
-			ui.Error(fmt.Sprintf("Found more than one network with the name '%s'. The name must be unique. Aborting.", config.NetworkName))
-			return multistep.ActionHalt
+	} else {
+		// Look up each network by it's name label
+		for vif, networkNameLabel := range config.NetworkName {
+			networks, err := client.GetNetworkByNameLabel(networkNameLabel)
+
+			if err != nil {
+				ui.Error(fmt.Sprintf("Error occured getting Network by name-label: %s", err.Error()))
+				return multistep.ActionHalt
+			}
+
+			switch {
+			case len(networks) == 0:
+				ui.Error(fmt.Sprintf("Couldn't find a network with the specified name-label '%s'. Aborting.", networkNameLabel))
+				return multistep.ActionHalt
+			case len(networks) > 1:
+				ui.Error(fmt.Sprintf("Found more than one network with the name '%s'. The name must be unique. Aborting.", networkNameLabel))
+				return multistep.ActionHalt
+			}
+
+			//we need the VIF index string
+			vifStr := fmt.Sprintf("%d", vif)
+			_, err = instance.ConnectNetwork(networks[0], vifStr)
+
+			if err != nil {
+				ui.Say(err.Error())
+			}
 		}
-
-		network = networks[0]
-	}
-
-	if err != nil {
-		ui.Say(err.Error())
-	}
-	_, err = instance.ConnectNetwork(network, "0")
-
-	if err != nil {
-		ui.Say(err.Error())
 	}
 
 	instanceId, err := instance.GetUuid()

xenserver/packer-plugin-xenserver

builder/xenserver/common/common_config.go
@@ -18,11 +18,11 @@ type CommonConfig struct {
 	Password string `mapstructure:"remote_password"`
 	HostIp   string `mapstructure:"remote_host"`
 
-	VMName        string   `mapstructure:"vm_name"`
-	VMDescription string   `mapstructure:"vm_description"`
-	SrName        string   `mapstructure:"sr_name"`
-	FloppyFiles   []string `mapstructure:"floppy_files"`
-	NetworkName   string   `mapstructure:"network_name"`
+	VMName        string            `mapstructure:"vm_name"`
+	VMDescription string            `mapstructure:"vm_description"`
+	SrName        string            `mapstructure:"sr_name"`
+	FloppyFiles   []string          `mapstructure:"floppy_files"`
+	NetworkNames  map[string]string `mapstructure:"network_names"`
 
 	HostPortMin uint `mapstructure:"host_port_min"`
 	HostPortMax uint `mapstructure:"host_port_max"`

builder/xenserver/iso/step_create_instance.go
@@ -109,7 +109,7 @@ func (self *stepCreateInstance) Run(state multistep.StateBag) multistep.StepActi
 
 	var network *xsclient.Network
 
-	if config.NetworkName == "" {
+	if len(config.NetworkNames) == 0 {
 		// No network has be specified. Use the management interface
 		network = new(xsclient.Network)
 		network.Ref = ""
@@ -141,35 +141,39 @@ func (self *stepCreateInstance) Run(state multistep.StateBag) multistep.StepActi
 			return multistep.ActionHalt
 		}
 
-	} else {
-		// Look up the network by it's name label
-
-		networks, err := client.GetNetworkByNameLabel(config.NetworkName)
+		_, err = instance.ConnectNetwork(network, "0")
 
 		if err != nil {
-			ui.Error(fmt.Sprintf("Error occured getting Network by name-label: %s", err.Error()))
-			return multistep.ActionHalt
+			ui.Say(err.Error())
 		}
 
-		switch {
-		case len(networks) == 0:
-			ui.Error(fmt.Sprintf("Couldn't find a network with the specified name-label '%s'. Aborting.", config.NetworkName))
-			return multistep.ActionHalt
-		case len(networks) > 1:
-			ui.Error(fmt.Sprintf("Found more than one network with the name '%s'. The name must be unique. Aborting.", config.NetworkName))
-			return multistep.ActionHalt
+	} else {
+		// Look up each network by it's name label
+		for i, networkNameLabel := range config.NetworkNames {
+			networks, err := client.GetNetworkByNameLabel(networkNameLabel)
+
+			if err != nil {
+				ui.Error(fmt.Sprintf("Error occured getting Network by name-label: %s", err.Error()))
+				return multistep.ActionHalt
+			}
+
+			switch {
+			case len(networks) == 0:
+				ui.Error(fmt.Sprintf("Couldn't find a network with the specified name-label '%s'. Aborting.", networkNameLabel))
+				return multistep.ActionHalt
+			case len(networks) > 1:
+				ui.Error(fmt.Sprintf("Found more than one network with the name '%s'. The name must be unique. Aborting.", networkNameLabel))
+				return multistep.ActionHalt
+			}
+
+			//we need the VIF index string
+			vifIndexString := fmt.Sprintf("%d", i)
+			_, err = instance.ConnectNetwork(networks[0], vifIndexString)
+
+			if err != nil {
+				ui.Say(err.Error())
+			}
 		}
-
-		network = networks[0]
-	}
-
-	if err != nil {
-		ui.Say(err.Error())
-	}
-	_, err = instance.ConnectNetwork(network, "0")
-
-	if err != nil {
-		ui.Say(err.Error())
 	}
 
 	instanceId, err := instance.GetUuid()
d9b571d5c5b7c8770c5148235cc3a3575c8f7257
Dongyu Zheng <garydzheng@gmail.com>
2016-10-26 22:02:30 -0700

    Convert uploaded XVAs from template to VM

--- builder/xenserver/xva/step_import_instance.go
+++ builder/xenserver/xva/step_import_instance.go
@@ -60,6 +60,12 @@ func (self *stepImportInstance) Run(state multistep.StateBag) multistep.StepActi
 	}
 	state.Put("instance_uuid", instanceId)
 
+	err = instance.SetIsATemplate(false)
+	if err != nil {
+		ui.Error(fmt.Sprintf("Error converting template to a VM: %s", err.Error()))
+		return multistep.ActionHalt
+	}
+
 	instance.SetDescription(config.VMDescription)
 	if err != nil {
 		ui.Error(fmt.Sprintf("Error setting VM description: %s", err.Error()))

ab68636892f04cd6141e71ef4d0bb5924f1b0d11
Dongyu Zheng <garydzheng@gmail.com>
2016-11-04 15:43:42 -0700

    Added cleanup to step_instantiate_template

--- builder/xenserver/xva/step_instantiate_template.go
+++ builder/xenserver/xva/step_instantiate_template.go
@@ -10,7 +10,6 @@ import (
 
 type stepInstantiateTemplate struct {
 	instance *xsclient.VM
-	vdi      *xsclient.VDI
 }
 
 func (self *stepInstantiateTemplate) Run(state multistep.StateBag) multistep.StepAction {
@@ -89,4 +88,57 @@ func (self *stepInstantiateTemplate) Run(state multistep.StateBag) multistep.Ste
 	return multistep.ActionContinue
 }
 
-func (self *stepInstantiateTemplate) Cleanup(state multistep.StateBag) {}
+func (self *stepInstantiateTemplate) Cleanup(state multistep.StateBag) {
+	config := state.Get("config").(config)
+	if config.ShouldKeepVM(state) {
+		return
+	}
+
+	ui := state.Get("ui").(packer.Ui)
+
+	// We want to perform a 'xe vm-uninstall'
+	// Uninstall a VM. This operation will destroy those VDIs that are marked RW and connected to this VM only.
+	if self.instance != nil {
+		vbds, err := self.instance.GetVBDs()
+		if err != nil {
+			ui.Error(err.Error())
+		}
+
+		// Destroy VDIs before destroying VM, since vm.Destroy() also destroys VBDs,
+		// in which case we will be unable to find the VDIs
+		for _, vbd := range vbds {
+			vbd_rec, err := vbd.GetRecord()
+			if err != nil {
+				ui.Error(err.Error())
+				continue
+			}
+			if vbd_rec["mode"].(string) == "RW" {
+				vdi, err := vbd.GetVDI()
+				if err != nil {
+					ui.Error(err.Error())
+					continue
+				}
+				vdi_vbds, err := vdi.GetVBDs()
+				if err != nil {
+					ui.Error(err.Error())
+					continue
+				}
+				// If connected to this VM only
+				if len(vdi_vbds) <= 1 {
+					ui.Say("Destroying VDI")
+					err = vdi.Destroy()
+					if err != nil {
+						ui.Error(err.Error())
+					}
+				}
+			}
+		}
+
+		ui.Say("Destroying VM")
+		_ = self.instance.HardShutdown()
+		err = self.instance.Destroy()
+		if err != nil {
+			ui.Error(err.Error())
+		}
+	}
+}

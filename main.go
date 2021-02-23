000bba48b330586e8e09def369b0333171b6d6d8
Daniel Koschützki <d.koschuetzki@hs-furtwangen.de>
2021-02-23 23:46:11 +0100

    added a new main with the new rpc server

---
+++ main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/xenserver/packer-builder-xenserver/builder/xenserver/iso"
+	"github.com/xenserver/packer-builder-xenserver/builder/xenserver/xva"
+
+	"github.com/hashicorp/packer-plugin-sdk/plugin"
+	"github.com/hashicorp/packer-plugin-sdk/version"
+)
+var (
+	// Version is the main version number that is being run at the moment.
+	Version = "0.3.0"
+
+	// VersionPrerelease is A pre-release marker for the Version. If this is ""
+	// (empty string) then it means that it is a final release. Otherwise, this
+	// is a pre-release such as "dev" (in development), "beta", "rc1", etc.
+	VersionPrerelease = "dev"
+
+	// PluginVersion is used by the plugin set to allow Packer to recognize
+	// what version this plugin is.
+	PluginVersion = version.InitializePluginVersion(Version, VersionPrerelease)
+)
+
+func main() {
+	pps := plugin.NewSet()
+	pps.RegisterBuilder("iso", new(iso.Builder))
+	pps.RegisterBuilder("xva", new(xva.Builder))
+	pps.SetVersion(PluginVersion)
+	err := pps.Run()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
